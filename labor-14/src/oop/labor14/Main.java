96c9795a9fcec605034d0cc1ed02bd10f5d3a414
Szanto Robert <szanto.robert@student.ms.sapientia.ro>
2024-05-27 14:52:45 +0300

    labor14

--- labor-14/src/oop/labor14/Main.java
+++ labor-14/src/oop/labor14/Main.java
@@ -1,10 +1,15 @@
 package oop.labor14;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> persons = new ArrayList<>();
+
+        persons.add(new Person("balint","laszlo",1999));
+        persons.add(new Person("Robert","Veress",2004));
+        Collections.sort(persons);
 
     }
 }
